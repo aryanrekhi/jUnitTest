a370656f227db23f50791e72430111d520c268a8
user <user@DESKTOP-9H6TJ4P>
2022-11-17 18:47:54 +0530

    jUnitTest

---
+++ src/jUnitTestPackage/jUnitFunctions.java
@@ -0,0 +1,14 @@
+package jUnitTestPackage;
+
+public class jUnitFunctions {
+	public static int addNumbers(int a,int b)
+	{  
+	return(a+b);
+	}
+
+	public static String addStrings(String x  , String y)
+	{
+		return(x+y);
+	}
+
+}
